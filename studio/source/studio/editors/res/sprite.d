2b98b51a73697a215238b31e672583029243e86d
Enalye <enalyen@gmail.com>
2024-05-23 01:56:15 +0200

    Outils déplacement/sélection/coin/côté

--- studio/source/studio/editors/res/sprite.d
+++ studio/source/studio/editors/res/sprite.d
@@ -7,6 +7,7 @@ module studio.editors.res.sprite;
 
 import std.file;
 import std.path;
+import std.math : abs;
 import atelier;
 import farfadet;
 import studio.editors.res.base;
@@ -22,6 +23,7 @@ final class SpriteResourceEditor : ResourceBaseEditor {
         Vec2u _imageSize;
         Previewer _preview;
         ParameterWindow _parameterWindow;
+        int _tool;
     }
 
     this(Farfadet ffd, Vec2f size) {
@@ -61,6 +63,9 @@ final class SpriteResourceEditor : ResourceBaseEditor {
         });
 
         _preview.addEventListener("clip", { _parameterWindow.setClip(_clip); });
+        _parameterWindow.addEventListener("tool", {
+            _tool = _parameterWindow.getTool();
+        });
     }
 
     private void _onSize() {
@@ -82,6 +87,8 @@ private final class ParameterWindow : UIElement {
     private {
         SelectButton _textureSelect;
         IntegerField[] _numFields;
+        ToolGroup _toolGroup;
+        int _tool;
     }
 
     this(string textureRID, Vec4u clip) {
@@ -89,6 +96,22 @@ private final class ParameterWindow : UIElement {
         foreach (entry; entries) {
 
         }*/
+
+        {
+            HBox hbox = new HBox;
+            hbox.setAlign(UIAlignX.right, UIAlignY.top);
+            hbox.setPosition(Vec2f(4f, 4f));
+            hbox.setSpacing(4f);
+            addUI(hbox);
+
+            _toolGroup = new ToolGroup;
+            foreach (key; ["selection", "move", "corner", "side"]) {
+                ToolButton btn = new ToolButton(_toolGroup,
+                    "editor:" ~ key ~ "-button", key == "selection");
+                btn.setSize(Vec2f(32f, 32f));
+                hbox.addUI(btn);
+            }
+        }
 
         VBox vbox = new VBox;
         vbox.setAlign(UIAlignX.left, UIAlignY.top);
@@ -137,6 +160,13 @@ private final class ParameterWindow : UIElement {
         addEventListener("draw", {
             Atelier.renderer.drawRect(Vec2f.zero, getSize(), Atelier.theme.surface, 1f, true);
         });
+
+        addEventListener("update", {
+            if (_toolGroup.value != _tool) {
+                _tool = _toolGroup.value;
+                dispatchEvent("tool", false);
+            }
+        });
     }
 
     string getTextureRID() const {
@@ -156,6 +186,10 @@ private final class ParameterWindow : UIElement {
         _numFields[3].value = clip.w;
         Atelier.ui.blockEvents = false;
     }
+
+    int getTool() const {
+        return _toolGroup.value();
+    }
 }
 
 private final class Previewer : UIElement {
@@ -164,29 +198,14 @@ private final class Previewer : UIElement {
         Sprite _sprite;
         float _zoom = 1f;
         SpriteResourceEditor _editor;
+        Vec2f _positionMouse = Vec2f.zero;
         Vec2f _deltaMouse = Vec2f.zero;
-        ToolGroup _toolGroup;
+        Vec2i _clipAnchor, _clipAnchor2;
+        bool _isResizingVertical;
     }
 
     this(SpriteResourceEditor editor) {
         _editor = editor;
-
-        {
-            HBox hbox = new HBox;
-            hbox.setAlign(UIAlignX.right, UIAlignY.top);
-            hbox.setPosition(Vec2f(0f, 4f));
-            hbox.setSpacing(4f);
-            addUI(hbox);
-
-            _toolGroup = new ToolGroup;
-            foreach (key; ["selection", "move", "corner", "side"]) {
-                ToolButton btn = new ToolButton(_toolGroup,
-                    "editor:" ~ key ~ "-button", key == "selection");
-                btn.setSize(Vec2f(32f, 32f));
-                btn.addEventListener("value", &_onToolChange);
-                hbox.addUI(btn);
-            }
-        }
     }
 
     void setTextureRID(string rid) {
@@ -212,8 +231,12 @@ private final class Previewer : UIElement {
             addEventListener("mouseup", &_onMouseUp);
             addEventListener("mouseleave", {
                 removeEventListener("mousemove", &_onDrag);
+                removeEventListener("mousemove", &_onMakeSelection);
+                _positionMouse = Vec2f.zero;
                 removeEventListener("mousemove", &_onMoveSelection);
                 _deltaMouse = Vec2f.zero;
+                removeEventListener("mousemove", &_onMoveCorner);
+                removeEventListener("mousemove", &_onMoveSide);
             });
         }
     }
@@ -225,10 +248,67 @@ private final class Previewer : UIElement {
             addEventListener("mousemove", &_onDrag);
             break;
         case left:
-            Vec4f clip = _zoom * cast(Vec4f) _editor._clip;
-            Vec2f origin = _sprite.position - _sprite.size / 2f + clip.xy;
-            if (getMousePosition().isBetween(origin, origin + clip.zw)) {
-                addEventListener("mousemove", &_onMoveSelection);
+            switch (_editor._tool) {
+            case 0:
+                _positionMouse = (getMousePosition() - (_sprite.position - _sprite.size / 2f)) / _zoom;
+                addEventListener("mousemove", &_onMakeSelection);
+                break;
+            case 1:
+                Vec4f clip = _zoom * cast(Vec4f) _editor._clip;
+                Vec2f origin = _sprite.position - _sprite.size / 2f + clip.xy;
+                if (getMousePosition().isBetween(origin, origin + clip.zw)) {
+                    addEventListener("mousemove", &_onMoveSelection);
+                }
+                break;
+            case 2:
+                Vec2f positionMouse = (getMousePosition() - (_sprite.position - _sprite.size / 2f)) / _zoom;
+                bool isResizingRight = positionMouse.x >= (_editor._clip.x + _editor._clip.z / 2f);
+                bool isResizingBottom = positionMouse.y >= (_editor._clip.y + _editor._clip.w / 2f);
+
+                _clipAnchor.x = _editor._clip.x + (isResizingRight ? 0 : _editor._clip.z);
+                _clipAnchor.y = _editor._clip.y + (isResizingBottom ? 0 : _editor._clip.w);
+
+                addEventListener("mousemove", &_onMoveCorner);
+                break;
+            case 3:
+                Vec2f positionMouse = (getMousePosition() - (_sprite.position - _sprite.size / 2f)) / _zoom;
+                bool isResizingRight = positionMouse.x >= (_editor._clip.x + _editor._clip.z / 2f);
+                bool isResizingBottom = positionMouse.y >= (_editor._clip.y + _editor._clip.w / 2f);
+
+                Vec2f delta = Vec2f.zero;
+                delta.x = positionMouse.x - cast(float)(_editor._clip.x + (isResizingRight ?
+                        _editor._clip.z : 0));
+                delta.y = positionMouse.y - cast(float)(_editor._clip.y + (isResizingBottom ?
+                        _editor._clip.w : 0));
+
+                _isResizingVertical = abs(delta.y) < abs(delta.x);
+
+                if (_isResizingVertical) {
+                    if (isResizingBottom) {
+                        _clipAnchor = Vec2i(_editor._clip.x, _editor._clip.y);
+                        _clipAnchor2 = Vec2i(_editor._clip.x + _editor._clip.z, _editor._clip.y);
+                    }
+                    else {
+                        _clipAnchor = Vec2i(_editor._clip.x, _editor._clip.y + _editor._clip.w);
+                        _clipAnchor2 = Vec2i(_editor._clip.x + _editor._clip.z,
+                            _editor._clip.y + _editor._clip.w);
+                    }
+                }
+                else {
+                    if (isResizingRight) {
+                        _clipAnchor = Vec2i(_editor._clip.x, _editor._clip.y);
+                        _clipAnchor2 = Vec2i(_editor._clip.x, _editor._clip.y + _editor._clip.w);
+                    }
+                    else {
+                        _clipAnchor = Vec2i(_editor._clip.x + _editor._clip.z, _editor._clip.y);
+                        _clipAnchor2 = Vec2i(_editor._clip.x + _editor._clip.z,
+                            _editor._clip.y + _editor._clip.w);
+                    }
+                }
+                addEventListener("mousemove", &_onMoveSide);
+                break;
+            default:
+                break;
             }
             break;
         default:
@@ -243,11 +323,45 @@ private final class Previewer : UIElement {
             removeEventListener("mousemove", &_onDrag);
             break;
         case left:
-            removeEventListener("mousemove", &_onMoveSelection);
-            _deltaMouse = Vec2f.zero;
+            switch (_editor._tool) {
+            case 0:
+                removeEventListener("mousemove", &_onMakeSelection);
+                _positionMouse = Vec2f.zero;
+                break;
+            case 1:
+                removeEventListener("mousemove", &_onMoveSelection);
+                _deltaMouse = Vec2f.zero;
+                break;
+            case 2:
+                removeEventListener("mousemove", &_onMoveCorner);
+                break;
+            case 3:
+                removeEventListener("mousemove", &_onMoveSide);
+                break;
+            default:
+                break;
+            }
             break;
         default:
             break;
+        }
+    }
+
+    private void _onMakeSelection() {
+        Vec2f endPositionMouse = (getMousePosition() - (_sprite.position - _sprite.size / 2f)) / _zoom;
+
+        Vec2f startClip = _positionMouse.min(endPositionMouse).floor();
+        Vec2f endClip = _positionMouse.max(endPositionMouse).ceil();
+
+        startClip = startClip.clamp(Vec2f.zero, cast(Vec2f) _editor._imageSize);
+        endClip = endClip.clamp(Vec2f.zero, cast(Vec2f) _editor._imageSize);
+
+        Vec4u clip = Vec4u(cast(uint) startClip.x, cast(uint) startClip.y,
+            cast(uint)(endClip.x - startClip.x), cast(uint)(endClip.y - startClip.y));
+
+        if (clip != _editor._clip) {
+            _editor._clip = clip;
+            dispatchEvent("clip", false);
         }
     }
 
@@ -279,6 +393,50 @@ private final class Previewer : UIElement {
         }
     }
 
+    private void _onMoveCorner() {
+        Vec2f mousePosition = (getMousePosition() - (_sprite.position - _sprite.size / 2f)) / _zoom;
+        mousePosition = mousePosition.clamp(Vec2f.zero, cast(Vec2f) _editor._imageSize);
+        Vec2i corner = cast(Vec2i) mousePosition;
+
+        Vec4i rect;
+        rect.xy = corner.min(_clipAnchor);
+        rect.zw = corner.max(_clipAnchor);
+
+        Vec4u clip;
+        clip.xy = cast(Vec2u) rect.xy;
+        clip.zw = cast(Vec2u)(rect.zw - rect.xy);
+
+        if (clip != _editor._clip) {
+            _editor._clip = clip;
+            dispatchEvent("clip", false);
+        }
+    }
+
+    private void _onMoveSide() {
+        Vec2f mousePosition = (getMousePosition() - (_sprite.position - _sprite.size / 2f)) / _zoom;
+        mousePosition = mousePosition.clamp(Vec2f.zero, cast(Vec2f) _editor._imageSize);
+        Vec2i point = cast(Vec2i) mousePosition;
+
+        Vec4u clip;
+        if (_isResizingVertical) {
+            clip.x = min(_clipAnchor.x, _clipAnchor2.x);
+            clip.z = max(_clipAnchor.x, _clipAnchor2.x) - clip.x;
+            clip.y = min(point.y, _clipAnchor.y);
+            clip.w = max(point.y, _clipAnchor.y) - clip.y;
+        }
+        else {
+            clip.x = min(point.x, _clipAnchor.x);
+            clip.z = max(point.x, _clipAnchor.x) - clip.x;
+            clip.y = min(_clipAnchor.y, _clipAnchor2.y);
+            clip.w = max(_clipAnchor.y, _clipAnchor2.y) - clip.y;
+        }
+
+        if (clip != _editor._clip) {
+            _editor._clip = clip;
+            dispatchEvent("clip", false);
+        }
+    }
+
     private void _onDrag() {
         UIManager manager = getManager();
         InputEvent.MouseMotion ev = manager.input.asMouseMotion();
@@ -302,9 +460,5 @@ private final class Previewer : UIElement {
         _sprite.size = (cast(Vec2f) _sprite.clip.zw) * _zoom;
         Vec2f delta = _sprite.position - getMousePosition();
         _sprite.position = delta * zoomDelta + getMousePosition();
-    }
-
-    private void _onToolChange() {
-
     }
 }

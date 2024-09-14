99b91543ebdfe1c39e619de7c61d8b34385a3457
Enalye <enalyen@gmail.com>
2024-09-14 19:48:42 +0200

    SelectButton utilise maintenant un système de défilement pour les grandes listes.

--- source/atelier/ui/select/list.d
+++ source/atelier/ui/select/list.d
@@ -7,8 +7,10 @@ module atelier.ui.select.list;
 
 import atelier.common;
 import atelier.core;
+import atelier.input;
 import atelier.render;
 import atelier.ui.core;
+import atelier.ui.scrollbar;
 import atelier.ui.select.button;
 import atelier.ui.select.item;
 
@@ -19,11 +21,20 @@ package final class SelectList : UIElement {
         uint _id;
         SelectButton _button;
         SelectItem[] _items;
+        VContentView _contentView;
+        VScrollbar _scrollbar;
     }
 
     this(SelectButton button) {
         _button = button;
         setAlign(UIAlignX.left, UIAlignY.top);
+
+        _contentView = new VContentView;
+        _contentView.setAlign(UIAlignX.left, UIAlignY.top);
+        _contentView.setChildAlign(UIAlignX.left);
+        _contentView.setSpacing(2f);
+        _contentView.setPosition(Vec2f(4f, 4f));
+        addUI(_contentView);
 
         _background = RoundedRectangle.fill(getSize(), Atelier.theme.corner);
         _background.color = Atelier.theme.foreground;
@@ -75,7 +86,6 @@ package final class SelectList : UIElement {
     private void _updateSize() {
         Vec2f padding = Vec2f(32f, 16f);
         Vec2f margin = Vec2f(4f, 4f);
-        float spacing = 2f;
 
         Vec2f newSize = Vec2f(padding.x, margin.y);
         float itemWidth = padding.x;
@@ -89,14 +99,54 @@ package final class SelectList : UIElement {
             item.setSize(Vec2f(itemWidth, item.getHeight()));
         }
 
-        foreach (UIElement child; getChildren()) {
-            child.setAlign(UIAlignX.left, UIAlignY.top);
-            child.setPosition(Vec2f(child.getPosition().x, newSize.y));
-            newSize.y += child.getHeight() + spacing;
+        const float maxHeight = 196f;
+
+        if (_contentView.getContentHeight() > maxHeight) {
+            _contentView.setSize(Vec2f(_contentView.getContentWidth(), maxHeight));
+
+            if (!_scrollbar) {
+                _scrollbar = new VScrollbar;
+                _scrollbar.setAlign(UIAlignX.right, UIAlignY.top);
+                _scrollbar.setWidth(9f);
+                _scrollbar.setPosition(Vec2f(1f, 6f));
+                addUI(_scrollbar);
+
+                addEventListener("wheel", &_onWheel);
+                _scrollbar.addEventListener("handlePosition", &_onHandlePosition);
+                _contentView.addEventListener("contentSize", &_onUpdateContent);
+            }
         }
-        newSize.y = max(padding.y, newSize.y + margin.y);
+        else {
+            _contentView.setSize(_contentView.getContentSize());
+        }
+
+        newSize.y = max(padding.y, _contentView.getHeight() + margin.y * 2f);
+
+        if (_scrollbar) {
+            newSize.x += _scrollbar.getWidth() + margin.x;
+            _scrollbar.setHeight(newSize.y - 12f);
+        }
 
         setSize(newSize);
+    }
+
+    private void _onHandlePosition() {
+        _contentView.setContentPosition(_scrollbar.getContentPosition());
+    }
+
+    private void _onUpdateContent() {
+        _scrollbar.setContentSize(_contentView.getContentHeight());
+    }
+
+    private void _onWheel() {
+        _scrollbar.removeEventListener("handlePosition", &_onHandlePosition);
+
+        UIManager manager = getManager();
+        InputEvent.MouseWheel ev = manager.input.asMouseWheel();
+        _contentView.setContentPosition(_contentView.getContentPosition() - ev.wheel.sum() * 32f);
+        _scrollbar.setContentPosition(_contentView.getContentPosition());
+
+        _scrollbar.addEventListener("handlePosition", &_onHandlePosition);
     }
 
     package void startRemove() {
@@ -116,7 +166,7 @@ package final class SelectList : UIElement {
         SelectItem item = new SelectItem(_button, itemName);
         item.setPosition(Vec2f(4f, 0f));
         _items ~= item;
-        addUI(item);
+        _contentView.addUI(item);
         _updateSize();
         return item;
     }

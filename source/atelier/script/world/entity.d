b596efd6270009d12c1400e0ff1a13d67057a6cd
Enalye <enalyen@gmail.com>
2025-10-26 20:56:56 +0100

    setShadow

--- source/atelier/script/world/entity.d
+++ source/atelier/script/world/entity.d
@@ -126,6 +126,7 @@ package void loadLibWorld_entity(GrModule mod) {
     mod.addFunction(&_isOnGround, "isOnGround", [entityType], [grBool]);
     mod.addFunction(&_hasGraphic, "hasGraphic", [entityType, grString], [grBool]);
     mod.addFunction(&_getBaseMaterial, "getBaseMaterial", [entityType], [grInt]);
+    mod.addFunction(&_setShadow, "setShadow", [entityType, grBool]);
 }
 
 private void _unregister(GrCall call) {
@@ -253,4 +254,9 @@ private void _hasGraphic(GrCall call) {
 private void _getBaseMaterial(GrCall call) {
     Entity entity = call.getNative!Entity(0);
     call.setInt(entity.getBaseMaterial);
+}
+
+private void _setShadow(GrCall call) {
+    Entity entity = call.getNative!Entity(0);
+    entity.setShadow(call.getBool(1));
 }
